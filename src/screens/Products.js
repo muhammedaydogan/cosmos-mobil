9f900c73a79bc74f8c0a44f55bbd4aff73704b75
muhammedaydogan <muhammed.aydogan@aol.com>
2021-04-27 15:29:29 +0300

    bug fixed

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,18 +1,17 @@
 import React from "react";
-
-import { Text } from "react-native";
+import axios from "axios";
+import { FlatList, StyleSheet, Text } from "react-native";
 
 export const Products = () => {
   return (
-    <Text
-      style={{
-        flex: 1,
-        color: "green",
-        justifyContent: "center",
-        fontSize: 16,
-      }}
-    >
-      SELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAMSELAM
-    </Text>
+    <>
+      <FlatList style={styles.flatList} />
+      {/* <Text style={styles.text}>SELAM</Text> */}
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  text: { flex: 1, color: "green", justifyContent: "center", fontSize: 16 },
+  flatList: { flex: 1, color: "orange" },
+});
